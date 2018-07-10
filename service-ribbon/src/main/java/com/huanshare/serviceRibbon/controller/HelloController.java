48b9ecb987ca2254c1d4c0304ede9e9b3d531f25
huan_liu <huan_liu@baofu.com>
2018-07-10 16:19:05 +0800

    添加zipkin-server,对接口调用进行监控(service-ribbon--->feign-service---->eureka-client)

--- service-ribbon/src/main/java/com/huanshare/serviceRibbon/controller/HelloController.java
+++ service-ribbon/src/main/java/com/huanshare/serviceRibbon/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.huanshare.serviceRibbon.controller;
 
 import com.huanshare.serviceRibbon.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * Created by liuhuan on 2018/6/11 17:58.
  */
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -16,6 +18,7 @@ public class HelloController {
     HelloService helloService;
     @RequestMapping(value = "/hi")
     public String hi(@RequestParam String name){
+        log.info("===call feign-service===");
         return helloService.hiService(name);
     }
 
---
+++ zipkin-server/readme.md
@@ -0,0 +1,16 @@
+# spring cloud版本问题
+由于spring cloud最新版本zipkin使用起来麻烦，特将相关版本降级为上一个版本
+
+- spring boot版本
+
+    1.5.9.RELEASE
+
+- spring cloud版本
+
+    Edgware.RC1
+    
+    
+
+- 接口调用服务顺序
+  
+  service-ribbon--->feign-service---->eureka-client
